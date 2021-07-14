efb6a827bd15e28dcdea4f75080951f251e734c5
wangjiangpeng <675650218@qq.com>
2021-07-14 22:58:43 +0800

    init

---
+++ app/src/main/java/com/wjp/english/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wjp.english;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+
+
+}
